41601b290b1dc01d66a84398dce59f91c89cd0f2
Pavel Uzuniants <holabeda@gmail.com>
2023-06-12 15:41:28 +0300

    feat: Сделал Header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./app.module.scss";
 import Main from "../pages/Main";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div className="App">
+    <>
+      <Header />
       <Main />
-    </div>
+    </>
   );
 };
 
